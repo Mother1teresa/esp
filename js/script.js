50d5caa5d9615988bb4bc3c92a256e9d1802f063
Mother1teresa <129117416+Mother1teresa@users.noreply.github.com>
2025-04-21 14:46:54 +0300

    done
    Блоки главной стр

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $("select").niceSelect();
+
+  $(".header__burger").on("click", function (evt) {
+    let $this = $(this);
+    $(".header__menu-container").slideToggle();
+  });
+
+  $(".areas-right__item").mouseover(function () {
+    const $this = $(this);
+    let animeId;
+    switch ($this.attr("class").split(" ")[1]) {
+      case "anim-one":
+        animeId = 0;
+        break;
+      case "anim-two":
+        animeId = 1;
+        break;
+      case "anim-three":
+        animeId = 2;
+        break;
+      case "anim-four":
+        animeId = 3;
+        break;
+    }
+    $(".areas-left__animation > div").each(function () {
+      $(this).css("display", "none");
+    });
+    $(".areas-left__animation > div")[animeId].style.display = "flex";
+  });
+});
