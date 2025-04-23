3cb0f1cff0e79552510a82fe752b10e1cbc925f2
Mother1teresa <129117416+Mother1teresa@users.noreply.github.com>
2025-04-23 18:55:23 +0300

    update шар

--- js/script.js
+++ js/script.js
@@ -28,4 +28,44 @@ $(document).ready(function () {
     });
     $(".areas-left__animation > div")[animeId].style.display = "flex";
   });
+
+
+
+
+});
+
+const targets = document.querySelectorAll('.rotating-target');
+const wrapper = document.querySelector('.ball-split');
+
+let animationFrame;
+let angle = 0;
+let isHovered = false;
+
+function rotateStep() {
+  if (!isHovered) return;
+
+  angle += 2;
+  targets.forEach(target => {
+    target.style.transform = `rotate(${angle}deg)`;
+  });
+  animationFrame = requestAnimationFrame(rotateStep);
+}
+
+wrapper.addEventListener('mouseenter', () => {
+  isHovered = true;
+  cancelAnimationFrame(animationFrame);
+  targets.forEach(target => {
+    target.style.transition = 'none'; // отключаем transition во время вращения
+  });
+  rotateStep();
+});
+
+wrapper.addEventListener('mouseleave', () => {
+  isHovered = false;
+  cancelAnimationFrame(animationFrame);
+  targets.forEach(target => {
+    target.style.transition = 'transform 1.2s ease';
+    target.style.transform = 'rotate(0deg)';
+  });
+  angle = 0;
 });
